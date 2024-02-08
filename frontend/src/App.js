5ef44b512b8fcad32eb303defb69b5417e9a0527
Svitlana Popova <svitlana.popova@docc.techstarter.de>
2024-02-08 16:10:29 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GoogleSignInButton from './components/GoogleSignInButton';
+
+function App() {
+  return (
+    <div className="App">
+      <GoogleSignInButton />
+    </div>
+  );
+}
+
+
+
+export default App;

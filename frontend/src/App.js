5ffe4c2b00dd3e1259b0c0205a09aff7d3c9b06c
Svitlana Popova <svitlana.popova@docc.techstarter.de>
2024-02-08 18:13:28 +0100

    meine hausaufgabe auth-google

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
-import GoogleSignInButton from './components/GoogleSignInButton';
+import React from "react";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import MeinBereich from "./components/MeinBereich";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
+ return (
     <div className="App">
-      <GoogleSignInButton />
+      // Unsere Liste von Routen
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='about' element={ <About/> } />
+        <Route path='login' element={ <Login/> } />
+        <Route path='myspace' element={ <MeinBereich/> } />
+      </Routes>
     </div>
   );
 }
 
-
-
-export default App;
+export default App
